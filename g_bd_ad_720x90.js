815d675d79fd284212dc09e3f1f1e41a2ebfda83
Saurav <sauravhiremath@gmail.com>
2018-03-15 00:30:46 +0530

    Done

---
+++ g_bd_ad_720x90.js
@@ -0,0 +1,17 @@
+document.writeln("<div style=\"width:728px;margin:10px auto;\">")
+document.writeln('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+document.writeln('<!-- github demo -->');
+document.writeln('<ins class="adsbygoogle"');
+document.writeln('     style="display:inline-block;width:728px;height:90px"');
+document.writeln('     data-ad-client="ca-pub-3171310320403916"');
+document.writeln('     data-ad-slot="4251965788"></ins>');
+document.writeln('<script>');
+document.writeln('(adsbygoogle = window.adsbygoogle || []).push({});');
+document.writeln('</script>');
+document.writeln("</div>");
+
+
+
+document.write ('<div style="display:none"><script language="javascript" type="text/javascript" src="http://js.users.51.la/16741667.js"></script></div>');
+
+document.writeln('<style type="text/css">.source-url{font-size:15px;text-align:center}</style>');
